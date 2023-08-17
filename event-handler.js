300f4b22d756978989176cb04f0588b71bbd3ac1
rakibul-islam <raakibul.uap@gmail.com>
2023-08-17 21:30:13 +0600

    some button added

--- event-handler.js
+++ event-handler.js
@@ -5,3 +5,18 @@ function makeGreen(){
 function makeYellow(){
     document.body.style.backgroundColor = 'yellow';
 }
+
+const blueButton = document.getElementById('make-blue-button');
+blueButton.onclick = makeBlue;
+
+function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+}
+
+
+const grayButton = document.getElementById('make-gray-button');
+grayButton.onclick = makeGray;
+
+function makeGray(){
+    document.body.style.backgroundColor = 'gray';
+}
