e5f93f999914ee6407dc92a9fd1994ea72602477
rakibul-islam <raakibul.uap@gmail.com>
2023-08-18 22:00:51 +0600

    Text box added

--- event-handler.js
+++ event-handler.js
@@ -44,5 +44,16 @@ function handleJustClickButton(){
 
 document.getElementById('direct-button').addEventListener('click', function(){
     const p = document.getElementById('clicking-method');
-    p.innerText = 'loremjjksdgkjsdgjksdjksfjksbdfjksbdgksbjk';
+    p.innerText = 'lorem';
+})
+
+
+document.getElementById('update-button').addEventListener('click', function(){
+    const nameField = document.getElementById('name-field');
+    const p = document.getElementById('clicking-method');
+    p.innerText = nameField.value; 
+    nameField.value = '';
+
+    // console.log(nameField.value);
+     
 })
