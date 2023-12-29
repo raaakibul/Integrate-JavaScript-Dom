df6b95d3031bda8577833cf483b284f8809eb6a8
rakibul-islam <raakibul.uap@gmail.com>
2023-12-29 23:32:06 +0600

    dom demo

--- Module-24/dom.js
+++ Module-24/dom.js
@@ -2,6 +2,11 @@ const blogTitles = document.getElementsByTagName('h3');
 // console.log(blogTitles);
 for(const h3 of blogTitles){
     console.log(h3);
-    console.log(h3.innerHTML);
+    // console.log(h3.innerHTML);
     console.log(h3.innerText);
+}
+
+const blogDetails = document.getElementsByTagName('p');
+for (const p of blogDetails){
+    console.log(p.innerText);
 }
