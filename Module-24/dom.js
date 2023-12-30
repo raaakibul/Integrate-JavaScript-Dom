0f47f2bb7d50a3e67d5f7e7c5b732e0c46ad407d
rakibul-islam <raakibul.uap@gmail.com>
2023-12-30 23:29:22 +0600

    javascript css

--- Module-24/dom.js
+++ Module-24/dom.js
@@ -3,11 +3,14 @@ const blogTitles = document.getElementsByTagName('h3');
 for(const h3 of blogTitles){
     console.log(h3);
     // console.log(h3.innerHTML);
-    console.log(h3.innerText);
+    // console.log(h3.innerText);
 }
 
 const blogDetails = document.getElementsByTagName('p');
 for (const p of blogDetails){
-    console.log(p);
-    console.log(p.innerText);
+    // console.log(p);
+    // console.log(p.innerText);
 }
+
+const secondTitle = document.getElementById('second-title');
+secondTitle.style.textAlign = 'center';
