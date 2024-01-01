2f5c87d3070d7009adb1ce9ec0b5b6180f84c894
rakibul-islam <raakibul.uap@gmail.com>
2024-01-01 21:31:34 +0600

    background color

--- Module-24/dom.js
+++ Module-24/dom.js
@@ -14,6 +14,7 @@ for (const p of blogDetails){
 
 const firstTitle = document.getElementById('first-title');
 firstTitle.style.color = 'red';
+firstTitle.style.backgroundColor = 'black';
 
 const secondTitle = document.getElementById('second-title');
 secondTitle.style.textAlign = 'center';
