28f0cdb31d6e2b9f3d4b0eb4bc8c111b8a156c2f
rakibul-islam <raakibul.uap@gmail.com>
2024-01-04 01:26:19 +0600

    undo comment

--- Module-24/dom.js
+++ Module-24/dom.js
@@ -2,14 +2,14 @@ const blogTitles = document.getElementsByTagName('h3');
 // console.log(blogTitles);
 for(const h3 of blogTitles){
     console.log(h3);
-    // console.log(h3.innerHTML);
-    // console.log(h3.innerText);
+    console.log(h3.innerHTML);
+    console.log(h3.innerText);
 }
 
 const blogDetails = document.getElementsByTagName('p');
 for (const p of blogDetails){
-    // console.log(p);
-    // console.log(p.innerText);
+    console.log(p);
+    console.log(p.innerText);
 }
 
 const firstTitle = document.getElementById('first-title');
