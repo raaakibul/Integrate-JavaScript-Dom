d8df468e32d8ff3fd9ea43af6ec84dfa8de04ad7
rakibul-islam <raakibul.uap@gmail.com>
2024-01-09 20:51:21 +0600

    event handler

--- Module-25/event.js
+++ Module-25/event.js
@@ -1,3 +1,14 @@
+// 2nd way
 function makeRed(){
     document.body.style.backgroundColor = 'red';
 }
+
+// 3rd way
+const blueButton = document.getElementById('blue-button');
+blueButton.onclick = makeBlue;
+
+function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+}
+
+
