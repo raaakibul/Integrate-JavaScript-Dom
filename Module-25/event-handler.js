63914078b76ac469b0322da9979c839322c5441c
rakibul-islam <raakibul.uap@gmail.com>
2024-01-10 20:11:28 +0600

    event handler added

--- Module-25/event-handler.js
+++ Module-25/event-handler.js
@@ -1,6 +1,9 @@
 function handleClick(){
     const clickingMethod = document.getElementById('clicking-method');
     clickingMethod.innerText = 'set by inner function';
-    
-
 }
+
+document.getElementById('handle-event').addEventListener('click', function(){
+    const p = document.getElementById('clicking-method');
+    p.innerText = 'set by event listener function';
+})
