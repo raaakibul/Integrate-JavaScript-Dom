6422be1fa0ba51299e80a9df3da1f3035feafa90
rakibul-islam <raakibul.uap@gmail.com>
2024-01-12 21:56:25 +0600

    input text

--- Module-25/event-handler.js
+++ Module-25/event-handler.js
@@ -10,12 +10,14 @@ document.getElementById('handle-event').addEventListener('click', function(){
     p.innerText = 'set by event listener function';
 })
 
+// add event listener connect input element to click handler 
+
 document.getElementById('update-name').addEventListener('click', function(){
     const nameField = document.getElementById('name-field');
     // console.log(nameField.value); input value return 
     const p = document.getElementById('clicking-method');
     p.innerText = nameField.value;
     nameField.value = '';
-    
+
 
 })
