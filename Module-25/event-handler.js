baaaf09eebb9aea4dd09194bdeca4e15975bf27f
rakibul-islam <raakibul.uap@gmail.com>
2024-01-13 21:14:23 +0600

    comment box using add event listener

--- Module-25/event-handler.js
+++ Module-25/event-handler.js
@@ -22,5 +22,21 @@ document.getElementById('update-name').addEventListener('click', function(){
 
 // Add comment 
 document.getElementById('post-button').addEventListener('click', function(){
+    // console.log("clicked");
+    // text area 
+    const commentText = document.getElementById('new-comment');
+    // console.log(commentText.value);
+    // const userComment = commentText.value;
+
+    const newComment = document.createElement('p');
+    newComment.innerText = commentText.value;
     
+    // append comment 
+    const commentContainer = document.getElementById('comment-container'); 
+    commentContainer.appendChild(newComment);
+    
+    commentText.value = '';
+    
+
+
 })
