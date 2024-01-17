ef61445497fc3402d644bd4055e76737570b606a
rakibul-islam <raakibul.uap@gmail.com>
2024-01-17 21:39:24 +0600

    focus event handler

--- Module-25/more-event.js
+++ Module-25/more-event.js
@@ -1,4 +1,7 @@
 document.getElementById("delete-btn").addEventListener("click", function(){
     document.getElementById("delete-info").style.display = "none";
-    
+})
+// focus 
+document.getElementById("delete-confirm").addEventListener('focus', function(){
+    document.body.style.backgroundColor = 'lightcoral';
 })
