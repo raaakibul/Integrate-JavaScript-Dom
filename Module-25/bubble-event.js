f4362c5702eadee8c4bc8120fa45787be3b827f0
rakibul-islam <raakibul.uap@gmail.com>
2024-01-22 21:13:32 +0600

    bubble list

---
+++ Module-25/bubble-event.js
@@ -0,0 +1,7 @@
+document.getElementById('second-item').addEventListener('click', function(){
+    console.log("second-item clicked");
+})
+
+document.getElementById('list-container').addEventListener('click', function(){
+    console.log("object");
+})
